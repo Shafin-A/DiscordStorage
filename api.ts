d14dff46d8ec0efc9b7b5849ae361d0d41813f9c
Shafin Ahmed <kazishafin@gmail.com>
2024-08-02 16:58:31 -0400

    Adding some null checks

--- api.ts
+++ api.ts
@@ -64,7 +64,7 @@ app.listen(PORT, () => {
   console.log(`Running on http://localhost:${PORT}`);
 });
 
-app.get("/files", async (req, res) => {
+app.get("/files", async (_req, res) => {
   // Login to Discord bot
   const client = new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent],
@@ -74,7 +74,7 @@ app.get("/files", async (req, res) => {
 
   // Get text channel - TODO
   const textChannelID = process.env.TEXT_CHANNEL_ID!;
-  const channel = (await client.channels.fetch(textChannelID))! as TextChannel;
+  const channel = (await client.channels.fetch(textChannelID)) as TextChannel;
 
   // Get all threads in channel
   const activeThreads = (await channel.threads.fetchActive()).threads;
@@ -88,15 +88,17 @@ app.get("/files", async (req, res) => {
     try {
       // Fetch the first & second message in the thread, messages fetch latest -> oldest
       const messages = await thread.messages.fetch({ limit: 2 });
-      const firstMessage = messages.last()!;
-
-      const secondMessage = messages.first()!;
+      const firstMessage = messages.last();
+      const secondMessage = messages.first();
 
       // First message is filename
-      const fileName = firstMessage.content;
+      const fileName = firstMessage ? firstMessage.content : "Unknown";
 
       // Second message is file size
-      const fileSize = secondMessage.content.split(" ")[0];
+      const fileSize = secondMessage
+        ? secondMessage.content.split(" ")[0]
+        : "Unknown";
+
       const threadID = thread.id;
 
       files.push({
@@ -128,111 +130,119 @@ app.get("/download/:threadID", async (req, res) => {
 
   // Get text channel - TODO
   const textChannelID = process.env.TEXT_CHANNEL_ID!;
-  const channel = (await client.channels.fetch(textChannelID))! as TextChannel;
+  const channel = (await client.channels.fetch(textChannelID)) as TextChannel;
 
   // Get thread
-  const thread = (await channel.threads.fetch(threadID))!;
+  const thread = await channel.threads.fetch(threadID);
 
-  // Get messages with attachments
-  const messages = await thread.messages.fetch();
+  if (thread) {
+    // Get messages with attachments
+    const messages = await thread.messages.fetch();
 
-  if (messages) {
-    // Within file size limit, no chunks
-    if (messages.size === 2) {
-      try {
-        // Latest message fetched first
-        const latestMessage = messages.first()!;
+    if (messages) {
+      // Within file size limit, no chunks
+      if (messages.size === 2) {
+        try {
+          // Latest message fetched first
+          const latestMessage = messages.first();
 
-        // Oldest message will be file name
-        const fileName = messages.last()!.content;
+          // Oldest message will be file name
+          const oldestMessage = messages.last();
+          const fileName = oldestMessage ? oldestMessage.content : "Unknown";
 
-        if (latestMessage.attachments.size === 1) {
-          const attachment = latestMessage.attachments.first();
-          const downloadURL = attachment!.url;
-          if (!downloadURL) {
-            return res.status(404).json({ error: "File not found" });
-          }
-
-          try {
-            // Fetch the file from the URL
-            const response = await fetch(downloadURL);
-
-            if (!response.ok) {
-              throw new Error(
-                `Failed to fetch ${downloadURL}: ${response.statusText}`
-              );
+          if (latestMessage && latestMessage.attachments.size === 1) {
+            const attachment = latestMessage.attachments.first();
+            if (!attachment) {
+              return res.status(404).json({ error: "File not found" });
             }
 
-            const contentType = response.headers.get("content-type");
+            const downloadURL = attachment.url;
 
-            // Set appropriate headers
-            res.setHeader(
-              "Content-Disposition",
-              `attachment; filename="${fileName}"`
-            );
-            res.setHeader(
-              "Content-Type",
-              contentType ? contentType : "Unknown"
-            );
+            try {
+              // Fetch the file from the URL
+              const response = await fetch(downloadURL);
 
-            // Download the file
-            if (!response.body) throw new Error("response.body is null");
-            await streamFile(response.body, res);
-          } catch (error) {
-            console.error("Error downloading file:", error);
-            res.status(500).json({ error: "Failed to download file" });
+              if (!response.ok) {
+                throw new Error(
+                  `Failed to fetch ${downloadURL}: ${response.statusText}`
+                );
+              }
+
+              const contentType = response.headers.get("content-type");
+
+              // Set appropriate headers
+              res.setHeader(
+                "Content-Disposition",
+                `attachment; filename="${fileName}"`
+              );
+              res.setHeader(
+                "Content-Type",
+                contentType ? contentType : "Unknown"
+              );
+
+              // Download the file
+              if (!response.body) throw new Error("response.body is null");
+              await streamFile(response.body, res);
+            } catch (error) {
+              console.error("Error downloading file:", error);
+              res.status(500).json({ error: "Failed to download file" });
+            }
+          } else {
+            console.log(
+              "The second message does not have exactly one attachment."
+            );
           }
-        } else {
+        } catch (error) {
+          console.error(
+            `Error fetching messages for thread ${threadID}:`,
+            error
+          );
+        }
+      }
+      // Larger than 25 MB case
+      else {
+        // Oldest message will be file name
+        const oldestMessage = messages.last();
+        const fileName = oldestMessage ? oldestMessage.content : "Unknown";
+
+        const downloadURLs: string[] = [];
+        messages.forEach((message) => {
+          if (message.attachments.size > 0) {
+            message.attachments.forEach((attachment) =>
+              downloadURLs.unshift(attachment.url)
+            );
+          }
+        });
+
+        const buffers = [];
+
+        for (let i = 0; i < downloadURLs.length; i++) {
           console.log(
-            "The second message does not have exactly one attachment."
+            `Building buffer ${i + 1}/${downloadURLs.length} for download...`
           );
+          const url = downloadURLs[i];
+
+          const response = await fetch(url);
+          const arrayBuffer = await response.arrayBuffer();
+          const buffer = Buffer.from(arrayBuffer);
+          buffers.push(buffer);
+
+          console.log(`Buffer ${i + 1}/${downloadURLs.length} done`);
         }
-      } catch (error) {
-        console.error(`Error fetching messages for thread ${threadID}:`, error);
-      }
-    }
-    // Larger than 25 MB case
-    else {
-      // Oldest message will be file name
-      const fileName = messages.last()!.content;
 
-      const downloadURLs: string[] = [];
-      messages.forEach((message) => {
-        if (message.attachments.size > 0) {
-          message.attachments.forEach((attachment) =>
-            downloadURLs.unshift(attachment.url)
-          );
-        }
-      });
+        // Concatenate all buffers into one
+        const responseBuffer = Buffer.concat(buffers);
 
-      const buffers = [];
-
-      for (let i = 0; i < downloadURLs.length; i++) {
-        console.log(
-          `Building buffer ${i + 1}/${downloadURLs.length} for download...`
+        // Set headers for file download
+        res.setHeader(
+          "Content-Disposition",
+          `attachment; filename="${fileName}"`
         );
-        const url = downloadURLs[i];
+        res.setHeader("Content-Type", "application/octet-stream");
 
-        const response = await fetch(url);
-        const arrayBuffer = await response.arrayBuffer();
-        const buffer = Buffer.from(arrayBuffer);
-        buffers.push(buffer);
-
-        console.log(`Buffer ${i + 1}/${downloadURLs.length} done`);
+        // Download
+        res.end(responseBuffer);
       }
-
-      // Concatenate all buffers into one
-      const responseBuffer = Buffer.concat(buffers);
-
-      // Set headers for file download
-      res.setHeader(
-        "Content-Disposition",
-        `attachment; filename="${fileName}"`
-      );
-      res.setHeader("Content-Type", "application/octet-stream");
-
-      // Download
-      res.end(responseBuffer);
     }
   }
 });
@@ -249,7 +259,7 @@ app.post("/upload", upload.single("file"), async (req, res) => {
 
   // Get text channel - TODO
   const textChannelID = process.env.TEXT_CHANNEL_ID!;
-  const channel = (await client.channels.fetch(textChannelID))! as TextChannel;
+  const channel = (await client.channels.fetch(textChannelID)) as TextChannel;
 
   // Setting max file sizes
   const fileBuffer = req.file.buffer;
@@ -269,7 +279,7 @@ app.post("/upload", upload.single("file"), async (req, res) => {
     });
 
     await thread.send(fileName);
-    await thread.send(`${req.file!.size} bytes`);
+    await thread.send(`${fileSize} bytes`);
 
     for (let i = 0; i < chunks.length; i++) {
       console.log(`Uploading chunk ${i + 1}/${chunks.length}...`);
@@ -302,7 +312,7 @@ app.post("/upload", upload.single("file"), async (req, res) => {
     );
 
     return res.json({
-      message: `File uploaded successfully in ${chunks.length} chunks: ${req.file.size} bytes`,
+      message: `File uploaded successfully in ${chunks.length} chunks: ${fileSize} bytes`,
       filename: req.file.originalname,
     });
   } else {
