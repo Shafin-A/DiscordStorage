b648cde4300e81725d6c51aaf5c36b1eddf280aa
Shafin Ahmed <kazishafin@gmail.com>
2024-08-05 01:16:02 -0400

    Add icons to sidebar

--- frontend/discord-storage/src/components/ui/sidebar.tsx
+++ frontend/discord-storage/src/components/ui/sidebar.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { Folder, Plus, UploadSimple } from "@phosphor-icons/react";
 
 const Sidebar = () => {
   return (
     <div className="flex min-h-screen w-full bg-muted/40 dark:bg-black">
-      <aside className="hidden w-64 flex-col border-r bg-background dark:bg-black p-4 sm:flex">
+      <aside className="hidden w-64 flex-col border-r dark:border-gray-600 bg-background dark:bg-black p-4 sm:flex">
         <div className="flex items-center gap-2 pb-4">
+          <Folder size={32} />
           <h2 className="text-lg font-semibold dark:text-white">
             File Manager
           </h2>
@@ -15,6 +17,7 @@ const Sidebar = () => {
             size="sm"
             className="justify-start gap-2 text-left dark:text-white"
           >
+            <Plus size={16} />
             New Folder
           </Button>
           <Button
@@ -22,14 +25,8 @@ const Sidebar = () => {
             size="sm"
             className="justify-start gap-2 text-left dark:text-white"
           >
+            <UploadSimple size={16} />
             Upload File
-          </Button>
-          <Button
-            variant="ghost"
-            size="sm"
-            className="justify-start gap-2 text-left dark:text-white"
-          >
-            Download
           </Button>
         </nav>
       </aside>
