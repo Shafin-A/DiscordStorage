f80121bece7ed6b4d7a3de7dff67e71fe02586f3
Shafin Ahmed <kazishafin@gmail.com>
2024-08-05 19:16:19 -0400

    Make header reuseable

--- frontend/discord-storage/src/App.tsx
+++ frontend/discord-storage/src/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
     <div className="flex min-h-screen w-full bg-muted/40 dark:bg-zinc-900">
       <Sidebar />
       <div className="flex-1 p-4 sm:p-6">
-        <Header />
+        <Header breadcrumbs={[{ href: "/", label: "Home" }]} page="Document" />
       </div>
     </div>
   );
--- frontend/discord-storage/src/components/ui/header.tsx
+++ frontend/discord-storage/src/components/ui/header.tsx
@@ -1,25 +1,47 @@
+import React from "react";
 import {
   Breadcrumb,
   BreadcrumbItem,
   BreadcrumbLink,
   BreadcrumbList,
   BreadcrumbPage,
+  BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
 import Sort from "@/components/ui/sort";
+import ModeToggle from "@/components/ui/modetoggle";
 
-import ModeToggle from "./modetoggle";
+type HeaderProps = {
+  breadcrumbs?: { href: string; label: string }[];
+  page?: string;
+};
 
-const Header = () => {
+const Header: React.FC<HeaderProps> = ({ breadcrumbs = [], page }) => {
   return (
     <header className="sticky top-0 z-10 flex h-14 items-center justify-between border-b dark:border-gray-600 dark:bg-zinc-950 px-4 sm:px-6 rounded-md">
-      <Breadcrumb>
-        <BreadcrumbList>
-          <BreadcrumbItem>
-            {/* <BreadcrumbLink href="/">Home</BreadcrumbLink> */}
-            <BreadcrumbPage>Home</BreadcrumbPage>
-          </BreadcrumbItem>
-        </BreadcrumbList>
-      </Breadcrumb>
+      {breadcrumbs.length > 0 && (
+        <Breadcrumb>
+          <BreadcrumbList>
+            {breadcrumbs.map((breadcrumb, index) => (
+              <div key={breadcrumb.href}>
+                {index > 0 && <BreadcrumbSeparator />}
+                <BreadcrumbItem>
+                  <BreadcrumbLink href={breadcrumb.href}>
+                    {breadcrumb.label}
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+              </div>
+            ))}
+            {page && (
+              <>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>{page}</BreadcrumbPage>
+                </BreadcrumbItem>
+              </>
+            )}
+          </BreadcrumbList>
+        </Breadcrumb>
+      )}
       <div className="flex gap-1">
         <ModeToggle />
         <Sort />
